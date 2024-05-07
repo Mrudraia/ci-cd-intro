6c91c6053072414487f12958596780cb2f385102
mrudraia <mrudraia@redhat.com>
2024-05-07 22:02:32 +0530

    few minor changes in routes

--- web/routes/users.js
+++ web/routes/users.js
@@ -6,4 +6,8 @@ router.get('/users', function (req, res, next) {
   res.send('respond with a resource')
 })
 
+router.get('/myusers', function (req, res, next) {
+  res.send('respond with a myusers resource')
+})
+
 module.exports = router
