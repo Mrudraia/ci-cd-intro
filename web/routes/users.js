296f080bec01620506394f63d2274ca2bf2af58c
mrudraia <mrudraia@redhat.com>
2024-06-27 20:06:55 +0530

    I am updating the routes for the users.js

--- web/routes/users.js
+++ web/routes/users.js
@@ -3,7 +3,7 @@ const router = express.Router()
 
 /* GET users listing. */
 router.get('/users', function (req, res, next) {
-  res.send('respond with a resource')
+  res.send('respond with a resource -- a sample text added to test PR')
 })
 
 module.exports = router
