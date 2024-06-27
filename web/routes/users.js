3e48881d72ecd0e81d97338b63c6bf4058e307a5
mrudraia <mrudraia@redhat.com>
2024-06-27 20:20:31 +0530

    routes modified

--- web/routes/users.js
+++ web/routes/users.js
@@ -3,7 +3,7 @@ const router = express.Router()
 
 /* GET users listing. */
 router.get('/users', function (req, res, next) {
-  res.send('respond with a resource -- a sample text added to test PR')
+  res.send('respond with a resource')
 })
 
 module.exports = router
